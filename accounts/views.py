22f2c24bf78ce96411874049cad47146f19d09a5
Nafis115 <20234103116@cse.bubt.edu.bd>
2024-07-04 21:15:38 +0600

    regi and login email

--- accounts/views.py
+++ accounts/views.py
@@ -5,25 +5,39 @@ from .forms import UserRegistrationForm,UserUpdateForm
 from django.urls import reverse_lazy
 from django.contrib.auth import login,logout
 from django.views import View
-
+from django.core.mail import EmailMessage,EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.contrib import messages
 
 # Create your views here.
+
+
+
 
 class UserRegisterView(FormView):
     template_name='accounts/register.html'
     form_class=UserRegistrationForm
-    success_url=reverse_lazy('register')
+    success_url=reverse_lazy('login')
     
     
     def form_valid(self, form):
         user=form.save()
+        messages.success(self.request,'Account create successfully')
+        self.send_account_email(user, 'Account Registration', 'accounts/account_created.html')
         return super().form_valid(form)
+    
+    def send_account_email(self, user, subject, template):
+        message = render_to_string(template, {'user': user})
+        send_email = EmailMultiAlternatives(subject, '', to=[user.email])
+        send_email.attach_alternative(message, "text/html")
+        send_email.send()
 
 class UserLoginView(LoginView):
     template_name='accounts/login.html'
     
     def get_success_url(self) :
-        return reverse_lazy('home')
+        messages.success(self.request,'Account create successfully')
+        return reverse_lazy('profile')
     
 class UserLogoutView(LogoutView):
     def get_success_url(self):
